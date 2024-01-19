d34bc4d0d9b45c492e887b0fc409e003fb7cd914
Luca Giovannesi <l.giovannesi@gmail.com>
2024-01-19 14:14:21 +0100

    main commited

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ import time
 import time
 from rpi_ws281x import PixelStrip
 from math import log2
+import os
 
-LED_COUNT = 300        # Number of LED pixels.
-LED_PIN = 21          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_COUNT = int(os.getenv('LED_COUNT'))        # Number of LED pixels.
+LED_PIN = 21                                    # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10                                  # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000                            # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10                                    # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255                            # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False                              # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0                                 # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # It is used as a library
-LED_FOR_SEGMENT = 2
+LED_FOR_SEGMENT = int(os.getenv("LED_PER_SEGMENT"))
 NUMBER_OF_SEGMENT = LED_COUNT // LED_FOR_SEGMENT
 NUMBER_OF_CHANNEL = 3 * NUMBER_OF_SEGMENT
 
-UNIVERSE = 1
+UNIVERSE = int(os.getenv("UNIVERSE"))
 
 class LedController:
 
